a00fdf6df240c100e129585ec0e79d6f8e689626
Tim Lang <langyong135@gmail.com>
2019-12-24 15:58:45 +0800

    Update 安卓支付回调

--- android/src/main/kotlin/com/pingpp/flutter_pingpp/FlutterPingppPlugin.kt
+++ android/src/main/kotlin/com/pingpp/flutter_pingpp/FlutterPingppPlugin.kt
@@ -1,18 +1,27 @@
 package com.pingpp.flutter_pingpp
 
-import com.pingplusplus.android.Pingpp;
-import android.app.Activity;
+import com.pingplusplus.android.Pingpp
+import android.app.Activity
+import android.content.Intent
+
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterPingppPlugin: MethodCallHandler {
+class FlutterPingppPlugin: MethodCallHandler, PluginRegistry.ActivityResultListener {
   private val _registrar: Registrar
+  private val METHOD_CHANNEL = "flutter_pingpp"
+  private val channel: MethodChannel
 
   constructor(registrar: Registrar) {
     this._registrar = registrar
+    this._registrar.addActivityResultListener(this)
+
+    channel = MethodChannel(registrar.messenger(), METHOD_CHANNEL)
+    channel.setMethodCallHandler(this)
   }
 
   companion object {
@@ -34,6 +43,24 @@ class FlutterPingppPlugin: MethodCallHandler {
 
   fun createPayment(currentActivity: Activity, call: MethodCall, result: Result) {
     var data = call.argument<String>("data")
+    print(">>>>>>>>>>>><<<<<<<<<<")
+    print(data)
     Pingpp.createPayment(currentActivity, data)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+    if (requestCode != Pingpp.REQUEST_CODE_PAYMENT) {
+      channel.invokeMethod("payFailed", null)
+      return false
+    } else {
+      var result = data?.getExtras()?.getString("pay_result")
+      if(result == "success") {
+        channel.invokeMethod("paySuccess", null)
+        return true
+      } else {
+        channel.invokeMethod("payFailed", null)
+        return false
+      }
+    }
   }
 }
--- lib/flutter_pingpp.dart
+++ lib/flutter_pingpp.dart
@@ -3,11 +3,31 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterPingpp {
-  static const MethodChannel _channel =
+  static const MethodChannel _methodChannel =
       const MethodChannel('flutter_pingpp');
 
-  static Future<String> createPayment(String data) async {
-    final String version = await _channel.invokeMethod('createPayment', { "data": data });
-    return version;
+  FlutterPingpp() {
+     _methodChannel.setMethodCallHandler(_callHandler);
+  }
+
+  Function _onPaySuccess;
+  Function _onPayError;
+
+
+  createPayment(String data, Function onSuccess, Function onError) async {
+    _onPaySuccess = onSuccess;
+    _onPayError = onError;
+    await _methodChannel.invokeMethod('createPayment', { "data": data });
+  }
+
+  Future<void> _callHandler(MethodCall call) async {
+    switch (call.method) {
+      case 'paySuccess':
+        _onPaySuccess(call.arguments);
+        break;
+      case 'payFailed':
+        _onPayError(call.arguments);
+        break;
+    }
   }
 }
