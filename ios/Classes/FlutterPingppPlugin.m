28cc8d72d056bea6bd94dd0acf0d292b3bc936d2
Tim Lang <langyong135@gmail.com>
2019-12-24 16:31:08 +0800

    Update iOS回调

--- ios/Classes/FlutterPingppPlugin.m
+++ ios/Classes/FlutterPingppPlugin.m
@@ -1,13 +1,23 @@
 #import "FlutterPingppPlugin.h"
 #import "Pingpp.h"
 
+@interface FlutterPingppPlugin ()
+@property(nonatomic, retain) FlutterMethodChannel *channel;
+@end
+
 @implementation FlutterPingppPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"flutter_pingpp"
             binaryMessenger:[registrar messenger]];
   FlutterPingppPlugin* instance = [[FlutterPingppPlugin alloc] init];
+  instance.channel = channel;
   [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary *)options {
+  BOOL canHandleURL = [Pingpp handleOpenURL:url withCompletion:nil];
+  return canHandleURL;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
@@ -21,13 +31,14 @@
     [Pingpp createPayment:data appURLScheme:@"O2BRA_APP" withCompletion:^(NSString *result2, PingppError *error) {
         NSLog(@"xxxxxxxxxxxxx");
         if (error == nil) {
-            NSLog(@">????????????");
+            [self.channel invokeMethod:@"paySuccess" arguments:nil];
         } else {
             NSLog(@"PingppError: code=%lu msg=%@", (unsigned  long)error.code, [error getMsg]);
+            [self.channel invokeMethod:@"payFailed" arguments:nil];
         }
     }];
 
-    result(data);
+    result(nil);
 }
 
 @end
